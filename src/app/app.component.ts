11f02ee946622e0980cf67f4880cf9aad4f80218
axlmemote <daniil.ustimencko@gmail.com>
2023-12-26 16:31:36 +0300

    main: in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,25 +19,33 @@ export class AppComponent {
       minScale: 0.02,
     });
 
-    document.querySelector('body')?.addEventListener('touchend', () => {
-      this.panzoom.pause();
-    });
+    // document.querySelector('body')?.addEventListener('touchend', () => {
+    //   this.panzoom.pause();
+    // });
 
     elem.parentElement.addEventListener('wheel', this.panzoom.zoomWithWheel);
   }
 
-  public getCurrentCoordinates(event: MouseEvent) {
+  public mi: any;
+  public mouseMove(event: any) {
+    clearTimeout(this.mi);
+    this.mi = setTimeout(() => {
+      this.getCurrentCoordinates(event);
+    }, 500);
+  }
+  public getCurrentCoordinates(event: any) {
     event.preventDefault();
-    console.log(event.offsetX, event.offsetY);
     const left = event.offsetX;
     const top = event.offsetY;
     let elem = this.world.nativeElement;
     try {
-      this.world.nativeElement.querySelector('.message').remove();
+      this.world.nativeElement
+        .querySelectorAll('.message')
+        .forEach((item: any) => item.remove());
     } catch (e) {}
 
     const instance = this.panzoom;
-    function createMessageUnder(elem: any, html: any) {
+    function createMessageUnder(elem: any, x: number = 0, y: number = 0) {
       // create message element
 
       let message = document.createElement('textarea');
@@ -45,42 +53,24 @@ export class AppComponent {
       message.style.cssText = 'position:absolute; color: black; font-size: 5px';
       message.classList.add('message');
 
-      message.style.left = ((left / 200) | 0) * 200 + 'px';
-      message.style.top = ((top / 200) | 0) * 200 + 'px';
+      let l = left + 200 * x;
+      let t = top + 200 * y;
+
+      if (l < 0) return;
+      if (t < 0) return;
+      message.style.left = ((l / 200) | 0) * 200 + 'px';
+      message.style.top = ((t / 200) | 0) * 200 + 'px';
       const coords = {
         left: message.style.left,
         top: message.style.top,
       };
       message.style.width = '200px';
       message.style.height = '200px';
-      message.style.border = '1px dotted black';
+      message.style.border = 'none';
+      message.style.outline = '0x solid red';
       message.style.padding = '52px';
       message.style.background = 'transparent';
-
-      message.addEventListener('mouseenter', (e) => {
-        document.querySelector('textarea')?.focus();
-        document.querySelector('textarea')!.value =
-          localStorage.getItem(JSON.stringify(coords)) || '';
-      });
-
-      message.addEventListener('touchmove', (e) => {
-        document.querySelector('textarea')?.focus();
-        document.querySelector('textarea')!.value =
-          localStorage.getItem(JSON.stringify(coords)) || '';
-      });
-
-      message.addEventListener('touchstart', (e) => {
-        document.querySelector('textarea')?.focus();
-        document.querySelector('textarea')!.value =
-          localStorage.getItem(JSON.stringify(coords)) || '';
-      });
-
-      message.addEventListener('change', () => {
-        localStorage.setItem(
-          JSON.stringify(coords),
-          document.querySelector('textarea')!.value
-        );
-      });
+      message.value = localStorage.getItem(JSON.stringify(coords)) || '';
 
       // message.innerHTML = html;
 
@@ -89,12 +79,13 @@ export class AppComponent {
 
     // Usage:
     // add it for 5 seconds in the document
-    let message = createMessageUnder(
-      elem,
-      `${((left / 200) | 0) * 200},${((top / 200) | 0) * 200}`
-    );
 
-    this.world.nativeElement.append(message);
+    for (let i = -50; i < 51; i++) {
+      for (let j = -50; j < 51; j++) {
+        let message = createMessageUnder(elem, i, j);
+        if (message) this.world.nativeElement.append(message);
+      }
+    }
   }
 
   public teleport() {
